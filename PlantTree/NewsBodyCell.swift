b68dbd58403e93180c72e33ef86bee1c8342b836
Hasan <hasan.akniyev@gmail.com>
2017-04-24 15:16:09 +0300

    Some changes in news cells

---
+++ PlantTree/NewsBodyCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsBodyCell.swift
+//  PlantTree
+//
+//  Created by Admin on 24/04/2017.
+//  Copyright © 2017 greenworld. All rights reserved.
+//
+
+import UIKit
+
+class NewsBodyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ PlantTree/NewsFooterCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsFooterCell.swift
+//  PlantTree
+//
+//  Created by Admin on 24/04/2017.
+//  Copyright © 2017 greenworld. All rights reserved.
+//
+
+import UIKit
+
+class NewsFooterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
