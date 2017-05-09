9c8b8203626cb92ad025f267217028f8d2cb772b
Unknown <hasan.akniyev@gmail.com>
2017-05-10 01:03:25 +0300

    Created new files for new button row

---
+++ PlantTree/RoundButtonCellNewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RoundButtonCellNewTableViewCell.swift
+//  PlantTree
+//
+//  Created by Hasan on 09/05/2017.
+//  Copyright © 2017 greenworld. All rights reserved.
+//
+
+import UIKit
+
+class RoundButtonCellNewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ PlantTree/RoundButtonRowNew.swift
@@ -0,0 +1,14 @@
+//
+//  RoundButtonRowNew.swift
+//  PlantTree
+//
+//  Created by Hasan on 09/05/2017.
+//  Copyright © 2017 greenworld. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class RoundButtonRowNew : Row<String>, RowType {
+    
+}
