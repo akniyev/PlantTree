94f0f35b0aacd8c9c2f6cf3c7e61d8fee0b0d470
Unknown <hasan.akniyev@gmail.com>
2017-05-09 14:29:46 +0300

    I've added ReloadIndicatorFooter

---
+++ PlantTree/ReloadIndicatorFooter.swift
@@ -0,0 +1,23 @@
+//
+//  ReloadIndicatorFooter.swift
+//  PlantTree
+//
+//  Created by Hasan on 09/05/2017.
+//  Copyright © 2017 greenworld. All rights reserved.
+//
+
+import UIKit
+
+class ReloadIndicatorFooter : UITableViewHeaderFooterView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+    }
+}
